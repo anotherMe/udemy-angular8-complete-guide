8e64b368dc6679c274cacb08a5e239ed4afb8ea4
marco <tuplanulla@gmail.com>
2020-02-09 18:17:37 +0100

    Lesson 135

--- section11-routing/src/app/users/user/user.component.ts
+++ section11-routing/src/app/users/user/user.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  user: { id: number, name: string };
+  routeSubscription: Subscription;
+
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.routeSubscription = this.route.params.subscribe( (params) => {
+
+      this.user = {
+        id: params['id'],
+        name: "Luca"
+      };
+
+    } );
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
   }
 
 }
