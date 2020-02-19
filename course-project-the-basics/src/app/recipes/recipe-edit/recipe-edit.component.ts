25c068fdef095b1e99997ddad1b16869dae55565
Marco Giordano <marco.giordano@microarea.it>
2020-02-19 17:25:22 +0100

    Lesson 229

--- course-project-the-basics/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project-the-basics/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,21 +12,61 @@ import { Recipe } from '../recipe.model';
 export class RecipeEditComponent implements OnInit {
 
   private currentRecipe: Recipe;
-  private editMode: boolean;
+  private recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, 
-    private recipeService: RecipeService) { 
-      this.editMode = false;
-    }
+  constructor(
+    private route: ActivatedRoute, 
+    private recipeService: RecipeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+
     this.route.params.subscribe(
       (params: Params) => {
-
-        this.currentRecipe = this.recipeService.getRecipe(+params['id']);
-        this.editMode = ( params['id'] != null );
+        if (params['id']) {
+          this.currentRecipe = this.recipeService.getRecipe(+params['id']);
+        } else { // we are editing a NEW recipe
+          this.currentRecipe = new Recipe(0, 'A new recipe', '', '', []);
+        }
       }
     );
+
+    this.initForm();
   }
 
+  private initForm() {
+
+    // create recipe form
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(this.currentRecipe.name),
+      'description': new FormControl(this.currentRecipe.description),
+      'imagePath': new FormControl(this.currentRecipe.imagePath),
+      'ingredients': new FormArray([])
+    });
+
+    // add ingredients to recipe form
+    for (const ingredient of this.currentRecipe.ingredients) {
+
+      const fg = new FormGroup({
+        'name': new FormControl(ingredient.name),
+        'amount': new FormControl(ingredient.amount)
+      });
+
+      (<FormArray>this.recipeForm.get('ingredients')).push(fg);
+    }
+
+  }
+
+  onSubmit() {
+    throw new Error('Not implemented');
+  }
+
+  onEditCancel() {
+    this.router.navigate(['recipes']);
+  }
+
+  onDeleteIngredient(index: number) {
+    console.log(`You asked to delete ingredient number: ${index}`);
+  }
 }
