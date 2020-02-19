74cc4eb7ea694bb841c48a105f76e971b6824f29
Marco Giordano <marco.giordano@microarea.it>
2020-02-19 10:06:31 +0100

    Lesson 276

--- section18-http-requests/src/app/auth-interceptor.service.ts
+++ section18-http-requests/src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
 
@@ -7,7 +8,11 @@ export class AuthInterceptorService implements HttpInterceptor {
 
         const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
         // return next.handle(req);
-        return next.handle(modifiedRequest);
+        return next.handle(modifiedRequest).pipe(
+            tap( event => {
+                console.log(event);
+            })
+        );
     }
 
 }
